dd16a6fbae5b6b0a5e183af95e2bd4640b6e132a
Bucerella <buseerkus@gmail.com>
2022-04-24 12:47:28 +0300

    Initial Commit

---
+++ DictionaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionaryApp
+//
+//  Created by Bucerella on 24.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
