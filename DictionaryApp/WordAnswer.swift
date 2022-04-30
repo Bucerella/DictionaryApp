48a7c7cc1511ba8649fa65788e0ec6ff5e0532b0
Bucerella <buseerkus@gmail.com>
2022-04-30 12:13:03 +0300

    Dictionary App

---
+++ DictionaryApp/WordAnswer.swift
@@ -0,0 +1,8 @@
+//
+//  WordAnswer.swift
+//  DictionaryApp
+//
+//  Created by Bucerella on 24.04.2022.
+//
+
+import Foundation
---
+++ DictionaryApp/Words.swift
@@ -0,0 +1,8 @@
+//
+//  Words.swift
+//  DictionaryApp
+//
+//  Created by Bucerella on 24.04.2022.
+//
+
+import Foundation
